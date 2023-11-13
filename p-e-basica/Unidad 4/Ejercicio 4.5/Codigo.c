c4df4d78a1a051f81f8e05257619106a5be6b80d
Ornella Bonachera <obonachera@undav.edu>
2023-11-13 08:58:08 -0300

    [Lunes - U4]

--- p-e-basica/Unidad 4/Ejercicio 4.5/Codigo.c
+++ p-e-basica/Unidad 4/Ejercicio 4.5/Codigo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct EVENTO{
     char Cod[10];
@@ -19,13 +20,19 @@ int main(){
     struct EVENTO E;
     struct EVENTOPROM EP;
 
-    char  Minimo[30];
+    char  Minimo[10];
     float Promedio;
     int   CantPorEvento=0;
     int   CantPuerta=0;
 
+    int Spuerta[15] = {0};
+    int Cpuerta[15] = {0};
+
     char CodAnt[10];
     int  PuertaAnt;
+
+    int Ban=0;
+    int Min;
 
     Archivo = fopen("eventos.dat","rb");
     ArchivoProm = fopen("eventopromedio.dat","wb");
@@ -34,31 +41,46 @@ int main(){
         exit(1);
     }
 
+    printf("---LEYENDO ARCHIVO---");
     fread(&E, sizeof(struct EVENTO), 1, Archivo);
     while(!feof(Archivo)){
             strcpy(CodAnt,E.Cod);
             CantPorEvento=0;
 
             while(strcmpi(CodAnt,E.Cod)==0 && !feof(Archivo)){
-                PuertaAnt = E.NumPuerta;
-                CantPuerta=0;
+                CantPorEvento+=E.CantEsp;
 
-                while(PuertaAnt == E.NumPuerta && strcmpi(CodAnt,E.Cod)==0 && !feof(Archivo)){
+                Spuerta[E.NumPuerta-1]+=E.CantEsp;
+                Cpuerta[E.NumPuerta-1]++;
 
-                    CantPuerta+=E.CantEsp;
-                    fread(&E, sizeof(struct EVENTO), 1, Archivo);
-                }
-                CantPorEvento+=CantPuerta;
+                fread(&E, sizeof(struct EVENTO), 1, Archivo);
+            }
 
-                EP.NumPuerta = PuertaAnt;
-                EP.Prom = (float)(CantPorEvento)/(CantPuerta);
-
-                fwrite(&EP,sizeof(struct EVENTOPROM),1,ArchivoProm);
+            if(Ban==0 && CantPorEvento < Min){
+                Ban=1;
+                Min = CantPorEvento;
+                strcpy(Minimo, E.Cod);
             }
             printf("Al evento de codigo %s asistieron %d espectadores.\n", CodAnt, CantPorEvento);
     }
 
+    printf("El evento %s obtuvo menor cantidad de espectadores.\n", Minimo);
+
     fclose(Archivo);
+
+    // Calculo del promedio
+
+    for(int i=0;i<15;i++){
+        if(Cpuerta[i]!=0){
+            EP.NumPuerta = i+1;
+            EP.Prom = (Spuerta[i] / Cpuerta[i])*(1.5);
+        }else{
+            EP.NumPuerta = i+1;
+            EP.Prom = 0;
+        }
+        fwrite(&EP,sizeof(struct EVENTOPROM),1,ArchivoProm);
+    }
+
     fclose(ArchivoProm);
 
     return 0;
